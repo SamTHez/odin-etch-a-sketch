fb7984ea76926ddb30389f1b6aa0c48b7408c0c5
SamTHez <samhezel@gmail.com>
2023-06-11 13:46:28 -0400

    Basic functionallity and UI is complete

--- index.js
+++ index.js
@@ -1,29 +1,72 @@
-const defaultSize = 20;
-const drawWindow = document.getElementById("drawWindow");
+const minSize = 1;
+const maxSize = 100;
+const defaultSize = 16;
+const drawColor = "#202020";
+const emptyColor = "#ffffff";
 
-function clearGrid(parent) {
-    while(parent.firstChild) {
-        parent.removeChild(parent.firstChild);
+const drawWindow = document.getElementById("drawWindow");
+const sizeInput = document.getElementById("sizeInput");
+const boxes = document.getElementsByClassName("gridBox");
+
+function getSizeInput() {
+    let input = sizeInput.value;
+
+    if(input >= minSize && input <= maxSize) {
+        return(input);
+
+    } else {
+        window.alert("Invalid Input\nPlease select a size between 1 and 100");
+        document.sizeInput = "";
+
+    }
+}
+
+function removeGrid() {
+    let gridRows = drawWindow.childNodes;
+    
+    while(gridRows[0]) {
+        gridRows[0].remove();
     }
 }
 
 function fillRow(row, size) {
     for(let i = 0; i < size; i++) {
-        var gridBox = document.createElement("div");
+        let gridBox = document.createElement("div");
         gridBox.className = "gridBox";
+        gridBox.addEventListener("mouseover", function() {colorBox(gridBox)});
         row.appendChild(gridBox);
+
     } 
 }
 
-function createGrid(size) {
-    window.alert("Hello World");
-    clearGrid(drawWindow);
+function createGrid() {
+    let size;
+    if(sizeInput.value.length == 0) {
+        size = defaultSize;
+    } else {
+        size = getSizeInput();
+    }
+
+    removeGrid();
+
     for(let i = 0; i < size; i++) {
-        var gridRow = document.createElement("div");
+        let gridRow = document.createElement("div");
         gridRow.className = "gridRow";
         fillRow(gridRow, size);
         drawWindow.appendChild(gridRow);
+
     } 
 }
 
-document.getElementById("testButton").addEventListener("onClick", createGrid(defaultSize));
+function colorBox(box) {
+    box.style.backgroundColor = drawColor;
+}
+
+function clearGrid() {
+    for(let i = 0; i < boxes.length; i++) {
+        boxes[i].style.backgroundColor = emptyColor;
+    }
+}
+
+document.getElementById("generateButton").addEventListener("click", createGrid);
+document.getElementById("clearButton").addEventListener("click", clearGrid);
